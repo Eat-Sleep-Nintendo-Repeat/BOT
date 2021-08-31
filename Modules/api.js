b176346ef82475b0cb4d41d2b6b670d67c2714f0
DustinDEV2more <public@dustin-dm.de>
2021-08-31 20:20:39 +0200

    add api axios config

---
+++ Modules/api.js
@@ -0,0 +1,15 @@
+const axios = require("axios")
+var config = require("../config.json")
+
+axios.interceptors.request.use(
+    (conf) => {
+        conf.headers["Authorization"] = `Token ${config["eat-sleep-nintendo-repeat-api"].api_key}`;
+        return conf;
+    },
+    (error) => {
+      Promise.reject(error);
+    }
+  );
+
+exports.axios = axios
+exports.baseURL = config["eat-sleep-nintendo-repeat-api"].base_url
