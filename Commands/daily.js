11edca833ad93841c84b9b5242ab1b768a05790b
Dustin Meyer <public@dustin-dm.de>
2021-11-22 19:35:24 +0100

    add gem emoji to daily message

--- Commands/daily.js
+++ Commands/daily.js
@@ -8,13 +8,17 @@ exports.command = {
 	async execute(interaction) {
         const {MessageEmbed} = require("discord.js")
         const {axios, baseURL} = require("../Modules/api")
+        const {client} = require("../index")
         const config = require("../config.json")
         await interaction.deferReply({ ephemeral: false })
 
-        //fetch api for daily
+        // fetch api for daily
         axios.post(baseURL + `/gems/${interaction.member.id}/daily`).then(res => {
-            interaction.editReply({embeds: [new MessageEmbed().setColor(config.colors.success).setTitle("Daily erfolgreich!").setDescription("Deine täglichen 150 <:EatSleepGem:912393376769376297> wurden geclaimt")]})
-        }).catch(e => {
+            
+            const Gemoji = client.emojis.cache.find(emoji => emoji.name === "EatSleepGem");
+            interaction.editReply({embeds: [new MessageEmbed().setColor(config.colors.success).setTitle("Daily erfolgreich!").setDescription(`Deine täglichen 150 ${Gemoji} wurden geclaimt`)]})
+    
+            }).catch(e => {
             if (e.response && e.response.data.message){
                 if (e.response.data.tryagain) { //Daily not ready
                     return interaction.editReply({embeds: [new MessageEmbed().setColor(config.colors.error).setTitle("API Error").setDescription(`Du hast deine tägliche Belohnung bereits eingelöst...\nVersuche es am ${new Date(e.response.data.tryagain).toLocaleDateString("DE-de")} um ${new Date(e.response.data.tryagain).toLocaleTimeString("DE-de")} Uhr erneut`)]})
