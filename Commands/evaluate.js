d1d20357e2049457d7a6b7cdc26c51a9dc800a1b
Dustin Meyer <public@dustin-dm.de>
2022-04-03 14:03:13 +0200

    remove non exsisting variables from evaluate

--- Commands/evaluate.js
+++ Commands/evaluate.js
@@ -15,9 +15,6 @@ exports.command = {
     permission: [Permissions.FLAGS.MANAGE_GUILD],
 
 	async execute(interaction) {
-        const message = interaction.message;
-        const guild = message.guild;
-        const channel = interaction.channel;
         const {MessageEmbed} = require("discord.js")
         const config = require("../config.json")
 
